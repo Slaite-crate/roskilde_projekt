4e4a141e6f5a6cd1378ac2dd647c46cfddd47a04
Pelle <pelle.park@gmail.com>
2020-03-19 18:01:42 +0100

    v1

--- src/com/roskildebørnehave/Venteliste.java
+++ src/com/roskildebørnehave/Venteliste.java
@@ -5,4 +5,25 @@ import java.util.Date;
 public class Venteliste {
     Date tilfoejelsesTidspunkt;
     Barn barn;
+
+    public Venteliste(Date tilfoejelsesTidspunkt, Barn barn) {
+        this.tilfoejelsesTidspunkt = tilfoejelsesTidspunkt;
+        this.barn = barn;
+    }
+
+    public Date getTilfoejelsesTidspunkt() {
+        return tilfoejelsesTidspunkt;
+    }
+
+    public void setTilfoejelsesTidspunkt(Date tilfoejelsesTidspunkt) {
+        this.tilfoejelsesTidspunkt = tilfoejelsesTidspunkt;
+    }
+
+    public Barn getBarn() {
+        return barn;
+    }
+
+    public void setBarn(Barn barn) {
+        this.barn = barn;
+    }
 }
