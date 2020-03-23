f9e532dc484eda73569a07d50450c39fbe90f3be
ceci138p <ceci138p@stud.kea.dk>
2020-03-23 10:12:17 +0100

    Person klasse begyndt

---
+++ src/com/roskildebørnehave/Person.java
@@ -0,0 +1,27 @@
+package com.roskildebørnehave;
+
+public abstract class Person {
+    private String fornavn;
+    private String efternavn;
+    private int cprNr;
+    private String adresse;
+    private String telefonnummer; // hvis man skal kunne skrive '+45' eller lign.
+    private String email;
+
+    public Person(String fornavn, String efternavn, int cprNr, String adresse, String telefonnummer, String email) {
+        this.fornavn = fornavn;
+        this.efternavn = efternavn;
+        this.cprNr = cprNr;
+        this.adresse = adresse;
+        this.telefonnummer = telefonnummer;
+        this.email = email;
+    }
+
+    public void tilføj(){}
+
+    @Override
+    public String toString() {
+        return fornavn + " " + efternavn + " ";
+    }
+
+}
