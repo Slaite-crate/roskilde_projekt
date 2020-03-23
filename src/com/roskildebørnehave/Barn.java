0040f84ba68ad205b27c7dfc29a9006d978aad8f
Pelle <pelle.park@gmail.com>
2020-03-23 14:43:59 +0100

    "v1"

--- src/com/roskildebørnehave/Barn.java
+++ src/com/roskildebørnehave/Barn.java
@@ -2,6 +2,8 @@ package com.roskildebørnehave;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.Date;
 
 public class Barn extends Person{
@@ -11,6 +13,7 @@ public class Barn extends Person{
 
     public Barn(String fornavn, String efternavn, int cprNr, String adresse, String telefonnummer, String email) {
         super(fornavn, efternavn, cprNr, adresse, telefonnummer, email);
+        this.alder = aldersOmregner(cprNr);
     }
 
     public int getAlder() {
@@ -28,16 +31,21 @@ public class Barn extends Person{
     public void setForaelder(Foraelder foraelder) {
         this.foraelder = foraelder;
     }
-    public int aldersOmregner(String cprNr){
-        String holdParameter = cprNr;
-        holdParameter = holdParameter.substring(0,6);
-        String nyDato = "20"+holdParameter.substring(4,6)+"-";
-        nyDato += holdParameter.substring(2,4);
+
+    public int aldersOmregner(int cprNr){
+        String holdParameter = Integer.toString(cprNr);
+        String nyDato = "20" + holdParameter.substring(4,6) + "-";
+        nyDato += holdParameter.substring(2,4) + "-";
+        nyDato += holdParameter.substring(4,6);
         String idag = dato();
-        return 0;
+        LocalDate start = LocalDate.parse(idag);
+        LocalDate slut = LocalDate.parse(nyDato);
+        Period p =  Period.between(slut, start);
+        return p.getYears();
     }
+
     public String dato(){
-        DateFormat df = new SimpleDateFormat("ddMMyy");
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
         Date idag = new Date();
         return df.format(idag);
     }
