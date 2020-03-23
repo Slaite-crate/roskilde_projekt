0c435e544cf4e19e7fe57389d8b99a8926a5041d
natalizacho <nata2716@stud.kea.dk>
2020-03-23 20:21:43 +0100

    hej

--- src/com/roskildebørnehave/Ansat.java
+++ src/com/roskildebørnehave/Ansat.java
@@ -6,6 +6,6 @@ public class Ansat extends Person {
     public Ansat(String fornavn, String efternavn, int cprNr, String adresse, String telefonnummer, String email) {
         super(fornavn, efternavn, cprNr, adresse, telefonnummer, email);
     }
-//hej
+
 
 }
