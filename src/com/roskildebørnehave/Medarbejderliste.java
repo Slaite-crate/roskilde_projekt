d0cd6027d04e34b2830f7506c864058684a4dbe5
Frederic <fred8442@stud.kea.dk>
2020-03-24 17:44:43 +0100

    hej

--- src/com/roskildebørnehave/Medarbejderliste.java
+++ src/com/roskildebørnehave/Medarbejderliste.java
@@ -39,16 +39,38 @@ public class Medarbejderliste  implements Liste {
         String email;
 
         fornavn = medarbejderListe.get(index).getFornavn();
+        efternavn = medarbejderListe.get(index).getEfternavn();
+        cprNr = medarbejderListe.get(index).getCprNr();
+        adresse = medarbejderListe.get(index).getAdresse();
+        telefonnummer = medarbejderListe.get(index).getTelefonnummer();
+        email = medarbejderListe.get(index).getEmail();
 
+        System.out.println("Tast 1 for fornavn, 2 for efternavn, 3 for cprNr, 4 for adresse, 5 for telefonnummer, 6 for email");
         int valg = input.nextInt();
         if (valg == 1){
+            System.out.println("Indtast nyt fornavn");
             fornavn = input.next();
         }
-        efternavn = input.next();
-        cprNr = input.nextInt();
-        adresse = "københavn";
-        telefonnummer = "123498765";
-        email = "noget@mail.com";
+        else if(valg == 2) {
+            System.out.println("Indtast nyt efternavn");
+            efternavn = input.next();
+        }
+        else if(valg == 3) {
+            System.out.println("Indtast nyt cprNr");
+            cprNr = input.nextInt();
+        }
+        else if(valg == 4) {
+            System.out.println("Indtast ny adresse");
+            adresse = input.next();
+        }
+        else if(valg == 5) {
+            System.out.println("Indtast nyt telefonnummer");
+            telefonnummer = input.next();
+        }
+        else if(valg ==5) {
+            System.out.println("Indtast ny email");
+            email = input.next();
+        }
 
         Ansat nyAnsat;
         nyAnsat = new Ansat(fornavn, efternavn, cprNr, adresse, telefonnummer, email);
